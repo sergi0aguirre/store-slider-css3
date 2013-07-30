af623948ad26a834f9721ea703db8b1a0c77cdd7
Sergio Aguirre <sergio@ievolutioned.com>
2013-07-30 11:33:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreSlider::Application.config.secret_token = '32208eeb412751012a0d5789781a2468f281aefd22cba889efa0252db94a5f9ff7a0a34fb6427e295b70e1905766f332a3d80d80a9c9371496c9d0253a6f16f3'
